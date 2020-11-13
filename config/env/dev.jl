0da6e5e64e60704d0321d09a24a43cf8a257d464
lansen.puttemans <8037dresT>
2020-11-13 14:06:45 +0100

    dev settings

--- config/env/dev.jl
+++ config/env/dev.jl
@@ -1,7 +1,7 @@
 using Genie.Configuration, Logging
 
 const config = Settings(
-  server_port                     = 8000,
+  server_port                     = $PORT,
   server_host                     = "0.0.0.0",
   log_level                       = Logging.Info,
   log_to_file                     = false,
