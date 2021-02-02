729d9761578d762cf2014bbb338590cd7c3d2019
lansen.puttemans <8037dresT>
2021-02-02 11:01:47 +0100

    add abm

--- routes.jl
+++ routes.jl
@@ -1,5 +1,15 @@
 using Genie.Router
+using AbmController
 
 route("/") do
   serve_static_file("welcome.html")
 end
+
+# added
+function hello_world()
+  "Hello World"
+end
+
+route("/hello", hello_world)
+
+route("/abm", AbmController.abm_run)
