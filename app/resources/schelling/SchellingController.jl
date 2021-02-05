b94b941f4ef8cd6efb9d3d6458d7ed5894cb8957
lansen.puttemans <8037dresT>
2021-02-05 18:29:22 +0100

    Abm coupling with web

--- app/resources/schelling/SchellingController.jl
+++ app/resources/schelling/SchellingController.jl
@@ -1,9 +1,16 @@
-module AbmController
+module SchellingController
 using Agents, Plots
+using Genie.Renderer.Html
 
-@agent SchellingAgent GridAgent{2} begin
+#=@agent SchellingAgent{Int} GridAgent{2} begin
     mood::Bool
     group::Int
+end=#
+mutable struct SchellingAgent <: AbstractAgent
+    id::Int # The identifier number of the agent
+    pos::Dims{2} # The x, y location of the agent on a 2D grid
+    mood::Bool # whether the agent is happy in its position. (true = happy)
+    group::Int # The group of the agent,  determines mood as it interacts with neighbors
 end
 
 space = GridSpace((10, 10), periodic = false)
@@ -67,6 +74,22 @@ function abm_run()
     data
 end
 
+struct Book
+  title::String
+  author::String
+end
+
+const BillGatesBooks = Book[
+  Book("The Best We Could Do", "Thi Bui"),
+  Book("Evicted: Poverty and Profit in the American City", "Matthew Desmond"),
+  Book("Believe Me: A Memoir of Love, Death, and Jazz Chickens", "Eddie Izzard"),
+  Book("The Sympathizer", "Viet Thanh Nguyen"),
+  Book("Energy and Civilization, A History", "Vaclav Smil")
+]
+
+function billgatesbooks()
+    html(:schelling, :schelling, books = BillGatesBooks)
+end
 #=
 groupcolor(a) = a.group == 1 ? :blue : :orange
 groupmarker(a) = a.group == 1 ? :circle : :square
--- bootstrap.jl
+++ bootstrap.jl
@@ -1,6 +1,7 @@
   cd(@__DIR__)
   import Pkg
   Pkg.activate(".")
+  using Revise
 
   function main()
     include(joinpath("src", "LoREcoSimulation.jl"))
